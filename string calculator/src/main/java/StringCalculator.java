ec9cc767f2ae84c743a48237bc9fd5bf70c5e5cd
jarjanazy <celil.cercenazi@gmail.com>
2020-11-26 17:36:35 +0200

    solve the questions from 2 to 4

--- string calculator/src/main/java/StringCalculator.java
+++ string calculator/src/main/java/StringCalculator.java
@@ -1,24 +1,49 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
+    private final String defaultDelimiter = ",";
 
-    public int checkAndAdd(String s){
+    public int checkLengthAndAdd(String s){
         String trimmedString = s.trim();
 
         if (trimmedString.isEmpty()) return 0;
         else if (trimmedString.length() == 1) return Integer.parseInt(trimmedString);
 
-        return add(trimmedString);
+        return checkDelimiterAndAddNumbers(trimmedString);
     }
 
-    private Integer add(String s) {
-        List<String> numbers = Arrays.asList(s.split(","));
+
+
+    private Integer checkDelimiterAndAddNumbers(String s) {
+        if (s.startsWith("//"))  {
+            String delimiter = s.substring(2, 3);
+            String usefulString = extractUsefulString(s);
+            return add(delimiter, usefulString);
+        }
+        else return add(defaultDelimiter, s);
+    }
+
+    private Integer add(String delimiter, String s) {
+        List<String> numbers = extractNumberAsStrings(delimiter, s);
+
         Optional<Integer> sum = numbers.stream().map(Integer::parseInt).reduce(Integer::sum);
 
         if (sum.isPresent()) return sum.get();
         else throw new RuntimeException("Something bad happened");
+    }
+
+    private List<String> extractNumberAsStrings(String delimiter, String s) {
+        String usefulString = s.replace("\n", delimiter);
+        return Arrays.asList(usefulString.split(Pattern.quote(delimiter))); // Pattern.quote to avoid problems with regex special keywords
+    }
+
+
+    private String extractUsefulString(String s) {
+        return s.substring(4);
+
     }
 
 }
--- string calculator/src/test/java/StringCalculatorTest.java
+++ string calculator/src/test/java/StringCalculatorTest.java
@@ -11,28 +11,59 @@ public class StringCalculatorTest {
 
     @Test
     void whenGivenStringIsEmpty_Return0(){
-        int result1 = stringCalculator.checkAndAdd("");
+        int result1 = stringCalculator.checkLengthAndAdd("");
         assertEquals(0, result1);
 
-        int result2 = stringCalculator.checkAndAdd(" ");
+        int result2 = stringCalculator.checkLengthAndAdd(" ");
         assertEquals(0, result2);
     }
 
     @Test
     void whenGivenStringHasOneNumber_ReturnIt(){
-        int result = stringCalculator.checkAndAdd("1");
+        int result = stringCalculator.checkLengthAndAdd("1");
         assertEquals(1, result);
 
-        int result2 = stringCalculator.checkAndAdd(" 1 ");
+        int result2 = stringCalculator.checkLengthAndAdd(" 1 ");
         assertEquals(1, result2);
     }
 
     @Test
     void whenGivenStringHasTwoNumbers_AddAndReturnThem(){
-        int result = stringCalculator.checkAndAdd("1,2");
+        int result = stringCalculator.checkLengthAndAdd("1,2");
         assertEquals(3, result);
-        int result2 = stringCalculator.checkAndAdd(" 5,5 ");
+        int result2 = stringCalculator.checkLengthAndAdd(" 5,5 ");
         assertEquals(10, result2);
+    }
+
+    @Test
+    void whenGivenStringHasMoreThanTwoNumber_AddAndReturnThem(){
+        int result = stringCalculator.checkLengthAndAdd("10,20,30");
+        assertEquals(60, result);
+
+        int result2 = stringCalculator.checkLengthAndAdd("1,10,100,1000");
+        assertEquals(1111, result2);
+    }
+
+    @Test
+    void whenGivenStringHasNewLinesAsDelimiter_AddAndReturnNumbers(){
+        int result = stringCalculator.checkLengthAndAdd("4\n6,10");
+        assertEquals(20, result);
+        int result2 = stringCalculator.checkLengthAndAdd("3\n20\n50,7");
+        assertEquals(80, result2);
+    }
+
+    @Test
+    void whenGivenStringHasCustomDelimiters_AddAndReturnNumbers(){
+        int result = stringCalculator.checkLengthAndAdd("//;\n10;0;6");
+        assertEquals(16, result);
+        int result2 = stringCalculator.checkLengthAndAdd("//.\n2.4.5");
+        assertEquals(11, result2);
+    }
+
+    @Test
+    void whenGivenStringHasCustomDelimitersAndNewLine_addAndReturnNumbers(){
+        int result = stringCalculator.checkLengthAndAdd("//|\n9|9\n9\n20");
+        assertEquals(47, result);
     }
 
 
